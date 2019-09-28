60b155273e7972291f0ffce27a6d5b0a7fc40a10
Danil <s48gs.w@gmail.com>
2019-09-29 00:46:48 +0300

    Add files via upload

---
+++ 2d_ex_physics/CPU_object.gd
@@ -0,0 +1,212 @@
+extends Sprite
+
+
+export var radius=0 # 512 256 64 32
+export var mass=0
+
+export var is_static=false # static does not move by gravity
+export var can_collide=true # collision enable
+
+# bounce include gravity vector and reflect it, false to disable
+export var reflect_gravity_on_hit=false
+export var bval=1.0 # 1.0 is bounce value, set <1 
+
+# enable/disable self gravity
+export var self_gravity=true
+
+# enable/disable self gravity
+export var react_gravity=true
+
+export var coll_level_3_active=false
+export var start_heading=Vector2()
+
+var is_active=true # if false this object ignored
+var heading=Vector2() #velocity
+
+# colors
+# black static object with gravity
+# red movable object with gravity, and do not react on other gravity
+# blue moving object without gravity
+# green moving object without gravity and collision
+
+#texture used to have mipmaps
+
+func _ready():
+	if(radius==512):
+		self.texture=load("res://round.png") as Texture
+	elif(radius==256):
+		self.texture=load("res://round2.png") as Texture
+	elif(radius==64):
+		self.texture=load("res://round3.png") as Texture
+	elif(radius==32):
+		self.texture=load("res://round4.png") as Texture
+	else:
+		radius=0
+	if(radius==0)||(mass==0):
+		is_active=false
+		self.visible=false
+	
+	var self_color=Color()
+	self.material=self.material.duplicate()
+	if(is_static):
+		self_color=Color.black
+	elif(can_collide)&&(self_gravity)&&(!react_gravity):
+		self_color=Color.red
+	elif(can_collide)&&(!self_gravity)&&(react_gravity):
+		self_color=Color.blue
+	elif(!can_collide)&&(!self_gravity)&&(react_gravity):
+		self_color=Color.green
+	
+	self.material.set("shader_param/color",self_color)
+	heading=start_heading
+
+func _process(delta):
+	if(!is_active):
+		return #can be free() node instead
+	sumVector()
+	slow_it(delta)
+	lim_heading()
+	self.position+=heading
+	
+	update_text()
+	process_input()
+
+func update_text():
+	self.material.set("shader_param/len",str(mass).length())
+	self.material.set("shader_param/value",int(mass))
+	self.material.set("shader_param/zoom_val",get_node("../").scale.x)
+
+# mass change by mouse
+func process_input():
+	if !(self_gravity&&is_active):
+		return
+	var iResolution=Vector2(1280,720)
+	var iMouse=get_viewport().get_mouse_position()/iResolution
+	iMouse=(iMouse-Vector2(0.5,0.5))*iResolution
+	if((self.position*get_node("../").scale-iMouse).length()<(radius/2)*get_node("../").scale.x):
+		get_node("../").mouse_block=true
+		if(Input.is_action_pressed("l_click")):
+			mass+=int(250+20000*smoothstep(5000,500000,mass))
+		if(Input.is_action_pressed("r_click")):
+			mass+=-int(250+20000*smoothstep(5000,500000,mass))
+		mass=clamp(mass,10,9999999)
+	
+
+# max speed limit, slowdown
+const max_speed=10
+func lim_heading():
+	if(heading.length()>max_speed):
+		heading=heading.normalized()*max_speed
+
+func angle2d(c,e):
+	var theta = atan2(e.y-c.y,e.x-c.x)
+	return theta
+
+# move back to center when position too far
+const max_length=10000
+func slow_it(delta):
+	if(self.position.length()>max_length):
+		var angl=-PI/2+angle2d(Vector2(),self.position)
+		var tva=max_speed*Vector2(sin(-angl),cos(-angl))
+		heading+=-tva*delta
+
+# void
+func distance_length(a,b):
+	return (a-b).length()
+
+# float
+func calculateGravity(body):
+	if(body==self):
+		return 0
+	var mass1=mass
+	var mass2=body.mass
+	var distanceBetween=distance_length(self.position,body.position)
+	distanceBetween=max(distanceBetween,0.0001)
+	return ((mass1 * mass2) / pow(distanceBetween, 2))
+
+# vec2
+func findVector(body):
+	var forceBetween = calculateGravity(body);
+	var hvDist=body.position-self.position
+	var totalDistance = abs(hvDist.x) + abs(hvDist.y);
+	totalDistance=max(totalDistance,0.0001)
+	return (forceBetween / totalDistance) * hvDist;
+
+# bool
+func collides(body):
+	var temp=self.position+heading
+	var distance = distance_length(temp,body.position);
+	if (distance < radius/2+body.radius/2):
+		return true
+	else:
+		return false
+
+# vec2
+func bounce(body,fi,dyn):
+	var tangentVector=Vector2()
+	tangentVector.y = -(body.position.x - self.position.x)
+	tangentVector.x = body.position.y - self.position.y
+	tangentVector = tangentVector.normalized()
+	var tfi=Vector2()
+	if(reflect_gravity_on_hit):
+		tfi=fi
+	var relativeVelocity = self.heading - body.heading + tfi
+	var leng = relativeVelocity.dot(tangentVector)
+	var velComponentOnTangent = leng*tangentVector
+	var velComponentPerpendicularToTangent=relativeVelocity - velComponentOnTangent
+	var ret=velComponentPerpendicularToTangent
+	if(dyn):
+		var tret=2*ret/(mass+body.mass)
+		body.heading+=tret*mass
+		return -tret*body.mass
+	else:
+		return -2*ret
+
+# vec2
+func unstuck(body):
+	var angl=-PI/2+angle2d(self.position,body.position)
+	var temp=self.position+heading
+	var distance = distance_length(temp,body.position);
+	var distanceToMove = radius/2 + body.radius/2 - distance
+	var tva=distanceToMove*Vector2(sin(-angl),cos(-angl))
+	self.position+=-tva
+
+# void
+func sumVector():
+	if(is_static):
+		return
+	var final=heading
+	for a in range(get_node("../").get_child_count()):
+		var el=get_node("../").get_child(a)
+		if(el==self)||(!el.is_active):
+			continue
+		if(!is_static)&&(react_gravity)&&(el.self_gravity):
+			var tv=findVector(el) # gravity
+			final+=tv
+		if(!is_static)&&(can_collide)&&(el.can_collide):
+			if(collides(el)): # collision
+				if(el.is_static):
+					final=heading+bounce(el,final,false)*bval # bounce static
+				else:
+					final=heading+bounce(el,final,true)*bval # bounce dyn
+				unstuck(el)
+				#break
+	heading=final
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 2d_ex_physics/CPU_phys.gd
@@ -0,0 +1,122 @@
+extends Node2D
+
+var elem
+
+var iTime=0.0
+
+var static_obj=Array()
+var static_orb_radius=Array()
+
+# GDScript can not calculate too much loops look like
+# same logic on C can display 500+ body very easy on my CPU(60fps)
+# when GDScript can not more then 100
+var static_size=6
+var dyn_grav_size=10*1
+var dyn_size=10*4
+var dyn_nograv_size=10*1
+
+var mouse_block=false
+
+func _ready():
+	# preload
+	elem=preload("res://CPU_object.tscn")
+	self.position=get_viewport().size/2
+	
+	# static gravity objects
+	spawn_1()
+	spawn_2()
+	spawn_3()
+	spawn_4()
+	
+	get_node("../Control/Label").text+=str(static_size+dyn_grav_size+dyn_size+dyn_nograv_size)+" body"
+	
+	
+
+func _process(delta):
+	iTime+=delta
+	process_input(delta)
+	process_static_move(delta)
+	mouse_block=false
+
+func spawn_1():
+	for a in range(static_size):
+		var s=elem.instance()
+		s.radius=512
+		s.mass=10000
+		s.is_static=true
+		s.can_collide=true
+		var an=PI/8*a
+		static_orb_radius.append(600*a+25*a*a)
+		s.position=(static_orb_radius[static_orb_radius.size()-1]*Vector2(sin(an),cos(an)))
+		static_obj.append(s)
+		self.call_deferred("add_child",s)
+
+func spawn_2():
+	for a in range(dyn_grav_size):
+		var s=elem.instance()
+		s.radius=256
+		s.mass=10000
+		s.is_static=false
+		s.can_collide=true
+		s.react_gravity=false
+		s.self_gravity=true
+		s.bval=1
+		var an=a*((PI*2)/dyn_grav_size)
+		s.position=(800+200*a)*Vector2(sin(an),cos(an))
+		self.call_deferred("add_child",s)
+
+func spawn_3():
+	for a in range(dyn_size):
+		var s=elem.instance()
+		s.radius=64
+		s.mass=1
+		s.is_static=false
+		s.can_collide=true
+		s.self_gravity=false
+		s.bval=0.98
+		var an=a*((PI*2)/dyn_size)
+		s.position=(650+10*a)*Vector2(sin(an),cos(an))
+		self.call_deferred("add_child",s)
+
+func spawn_4():
+	for a in range(dyn_nograv_size):
+		var s=elem.instance()
+		s.radius=32
+		s.mass=1
+		s.is_static=false
+		s.can_collide=false
+		s.self_gravity=false
+		s.bval=0.98
+		var an=a*((PI*2)/dyn_nograv_size)
+		s.position=(750+10*a)*Vector2(sin(an),cos(an))
+		self.call_deferred("add_child",s)
+
+func process_input(delta):
+	if(Input.is_action_pressed("zoom_out")||(Input.is_action_pressed("l_click")&&(!mouse_block))):
+		self.scale=self.scale+Vector2(delta*2.0,delta*2.0)*self.scale
+		self.scale=Vector2(min(self.scale.x,1),min(self.scale.y,1))
+	if(Input.is_action_pressed("zoom_in")||(Input.is_action_pressed("r_click")&&(!mouse_block))):
+		self.scale=self.scale-Vector2(delta*2.0,delta*2.0)*self.scale
+		self.scale=Vector2(max(self.scale.x,0.01),max(self.scale.y,0.01))
+	
+
+func angle2d(c,e):
+	var theta = atan2(e.y-c.y,e.x-c.x)
+	return theta
+
+# move on circle
+func process_static_move(delta):
+	for a in range(static_size):
+		var angl=-PI/2+angle2d(Vector2(),static_obj[a].position)
+		angl+=(0.81-0.7*smoothstep(0,static_size,a))*delta
+		var tva=static_orb_radius[a]*Vector2(sin(-angl),cos(-angl))
+		static_obj[a].heading=-static_obj[a].position+tva
+
+
+
+
+
+
+
+
+
