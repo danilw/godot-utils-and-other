ab7d8b8b646818897fab62acd4d8a7353f283497
Danil <s48gs.w@gmail.com>
2021-01-16 15:50:21 +0300

    win fix

--- cube_ex_tools/frame_capture/custom_shape.gd
+++ cube_ex_tools/frame_capture/custom_shape.gd
@@ -84,7 +84,7 @@ func on_save():
   
   var img = Image.new()
   img.create_from_data(image_size.x, image_size.y, false, Image.FORMAT_RGB8, byte_array)
-  img.save_png(path+"/capture.png")
+  img.save_png(path.plus_file("capture.png"))
   print("done")
   saved_text.visible=true
 
